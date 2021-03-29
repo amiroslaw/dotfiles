5ec9190dc01581285bc30f242210b75826c979de
amiroslaw <arek47@gmail.com>
2021-03-29 20:50:07 +0200

    add pqiv config; change vifm

--- common/rofi/.config/rofi/scripts/locate-video.py
+++ common/rofi/.config/rofi/scripts/locate-video.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-fd --follow --full-path --type=f -e mp4 -e mkv -e avi -e 4v -e mkv -e webm -e 3u -e mv -e pg ~/Videos | rofi -threads 0 -width 100 -lines 30 -multi-select -dmenu -i -p "locate:" | awk '{print "\""$0"\""}' | xargs -r $VIDEO
+fd --follow  --type=f -e mp4 -e mkv -e avi -e 4v -e mkv -e webm -e 3u -e mv -e pg --search-path ~/Videos | rofi -width 100 -lines 30 -multi-select -dmenu -i -p "locate:" | awk '{print "\""$0"\""}' | xargs -r $VIDEO
 
+# fd --follow --full-path --type=f -e mp4 -e mkv -e avi -e 4v -e mkv -e webm -e 3u -e mv -e pg ~/Videos | rofi -threads 0 -width 100 -lines 30 -multi-select -dmenu -i -p "locate:" | awk '{print "\""$0"\""}' | xargs -r $VIDEO
 # find
 # find -L ~/Videos -type f | rofi -threads 0 -width 100 -lines 30 -multi-select -dmenu -i -p "locate:" | awk '{print "\""$0"\""}' | xargs -r xdg-open
 # xdg-open "$(find -L ~/Videos -type f | rofi -threads 0 -width 100 -lines 30 -multi-select -dmenu -i -p "locate:")"
