6d9688290fc8a73320187d433496852a1e22ddfe
amiroslaw <arek47@gmail.com>
2021-04-26 00:55:55 +0200

    add taskbar to polybar

---
+++ common/scripts/.config/polybar/taskbar.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "$(bspc query -N -n .window -d focused)" ]; then
+    echo "%{B#323232}%{F#d0d0d0} --- %{u-}"
+fi
+
+for WINDOW in $(bspc query -N -n .window -d focused)
+do
+    FOCUS=`bspc query -N -n`
+    [[ "$FOCUS" == "$WINDOW" ]] && COLOR="#222" || COLOR="#323232"
+    TITLE=$(xdotool getwindowname $WINDOW | sed 's/^.*-\s//g' | sed -E 's/(–.*$)?(\s?\(.*\))?//g')
+    MAX_LENGTH=$(((${#TITLE}*5)/100))
+    [ $MAX_LENGTH -gt 0 ] && TITLE=`echo $TITLE | sed -E "s/.{$MAX_LENGTH}$/.../"` || TITLE=$TITLE
+    echo -e "%{A1:bspc node -f $WINDOW:}%{B$COLOR}%{F#d0d0d0}  $TITLE%{A}%{u-} \c"
+done
+
