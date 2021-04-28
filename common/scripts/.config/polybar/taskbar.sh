f9e207beaa564b9c40754f8f5d2f630dad3b63e0
amiroslaw <arek47@gmail.com>
2021-04-28 02:45:46 +0200

    add multimonitor support for taskbar

--- common/scripts/.config/polybar/taskbar.sh
+++ common/scripts/.config/polybar/taskbar.sh
@@ -1,11 +1,61 @@
 #!/usr/bin/env bash
-# https://github.com/nautilor/polybar-bspwm-taskbar-module
+# https://github.com/nocturnalbeast/polybar-bspwm-taskbar-module
+# exec = taskbar --activebg "#000" --inactivebg "#303030" --fg "#eaeaea" --separator "·" --monitor
+# add moniotr name if you want to have multimoniotor support; 
+# with some monitors name it can be a problem https://github.com/baskerville/bspwm/issues/552
+# DVI-D-0
+# DVI-I-0
 
+ACTIVE_BG_COLOR="#323232"
+INACTIVE_BG_COLOR="#222"
+FG_COLOR="#d0d0d0"
+SEPARATOR=""
+MONITOR=""
+
+while [[ $# -gt 0 ]]; do
+    KEY="$1"
+    case $KEY in
+        --activebg) ACTIVE_BG_COLOR="$2";;
+        --inactivebg) INACTIVE_BG_COLOR="$2";;
+        --fg) FG_COLOR="$2";;
+        --separator) SEPARATOR="$2";;
+        --monitor) MONITOR="$2";;
+        *) echo "Unknown option $KEY.";;
+    esac
+    shift
+    shift
+done
+LAST_DESKTOP=1
+
+while read LINE; do
+	MONITOR_FOCUSED=$(bspc query -N -n .window -d focused -m $MONITOR)
+	if [[ -z $MONITOR_FOCUSED ]]
+	then
+		mapfile -t WINDOW_LIST < <( bspc query -N -n .window -d $LAST_DESKTOP -m $MONITOR )
+	else
+		mapfile -t WINDOW_LIST < <( bspc query -N -n .window -d focused -m $MONITOR )
+		LAST_DESKTOP=$(bspc query -D -d focused -m $MONITOR)
+	fi
+    if [ ${#WINDOW_LIST[@]} -eq 0 ]; then
+        echo "%{B$INACTIVE_BG_COLOR}%{F$FG_COLOR} - %{u-}"
+    else
+        MODULE_STR=""
+        for WINDOW in "${WINDOW_LIST[@]}"; do
+            FOCUS="$( bspc query -N -n )"
+            [[ "$FOCUS" == "$WINDOW" ]] && BG_COLOR="$ACTIVE_BG_COLOR" || BG_COLOR="$INACTIVE_BG_COLOR"
+            TITLE="$( xdotool getwindowname $WINDOW | sed 's/^.*-\s//g' | sed -E 's/(–.*$)?(\s?\(.*\))?//g' )"
+            MAX_LENGTH=$(((${#TITLE}*5)/100))
+            [ $MAX_LENGTH -gt 0 ] && TITLE="$( echo $TITLE | sed -E "s/.{$MAX_LENGTH}$/.../" )" || TITLE=$TITLE
+            MODULE_STR="${MODULE_STR}%{A1:bspc node -f $WINDOW:}%{B$BG_COLOR}%{F$FG_COLOR} $SEPARATOR $TITLE%{A}%{u-} "
+        done
+        echo -e "$MODULE_STR\n\c"
+    fi
+done < <( echo && bspc subscribe node_focus node_add node_remove desktop_focus monitor_focus )
 if [ -z "$(bspc query -N -n .window -d focused)" ]; then
 	echo "%{B#323232}%{F#d0d0d0} - %{u-}"
 fi
 
-for WINDOW in $(bspc query -N -n .window -d focused)
+for WINDOW in $(bspc query -N -n .window -d focused -m $MONITOR)
 do
 	FOCUS=`bspc query -N -n`
 	[[ "$FOCUS" == "$WINDOW" ]] && COLOR="#222" || COLOR="#323232"
@@ -15,9 +65,3 @@ do
 	echo -e "%{A1:bspc node -f $WINDOW:}%{B$COLOR}%{F#d0d0d0}  $TITLE%{A}%{u-} \c"
 done
 
-# taskbar() {
-# }
-
-# bspc subscribe report | while read -r line; do
-#     taskbar
-# done
