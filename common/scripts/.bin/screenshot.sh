b784ef65d0607839e18b9154e6f13ebb7e63bdd9
amiroslaw <arek47@gmail.com>
2021-04-28 03:46:57 +0200

    add screenshot button into polybar

--- common/scripts/.bin/screenshot.sh
+++ common/scripts/.bin/screenshot.sh
@@ -31,6 +31,12 @@ case "$1" in
   'clipboard')
     maim -i $(xdotool getactivewindow) | xclip -selection clipboard -t image/png
   ;;
+  'monitor1')
+	maim -g $(xrandr --listactivemonitors | grep '+' | awk '{print $4, $3}' | awk -F'[x/+* ]' 'NR==1 {print $2"x"$4"+"$6"+"$7}') "$DIR"/"$STAMP"_Fsc.png
+  ;;
+  'monitor2')
+	maim -g $(xrandr --listactivemonitors | grep '+' | awk '{print $4, $3}' | awk -F'[x/+* ]' 'NR==2 {print $2"x"$4"+"$6"+"$7}') "$DIR"/"$STAMP"_Fsc.png
+  ;;
   *)
 	maim -i $(xdotool getactivewindow) "$DIR"/"$STAMP"_sc.png
   ;;
