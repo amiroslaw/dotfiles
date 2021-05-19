40c9a98b6228802be42af6a2b56f00a9369e3197
amiroslaw <arek47@gmail.com>
2021-05-19 03:17:31 +0200

    add kindle script

--- common/scripts/.bin/magnet.sh
+++ common/scripts/.bin/magnet.sh
@@ -2,8 +2,13 @@
 ## usage: ./download_magnet_url.sh "magnet_link"
 ## magnet link shoul be enclosed in quotes.
 
+link=$1
+if [ -z "$1" ]
+  then
+	link=$(xclip -o -sel clip)
+fi
 cd $TOR_WATCH # set your watch directory here
-[[ "$1" =~ xt=urn:btih:([^&/]+) ]] || exit;
+[[ "$link" =~ xt=urn:btih:([^&/]+) ]] || exit;
 echo "d10:magnet-uri${#1}:${1}e" > "meta-${BASH_REMATCH[1]}.torrent"
 
 notify-send "⬇️  Start downloading File 📁"
