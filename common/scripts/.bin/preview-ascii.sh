66561aa66e0a136994c168a6dbb411393f8497aa
amiroslaw <arek47@gmail.com>
2021-09-21 01:30:39 +0200

    add attrs to asciidoctor

--- common/scripts/.bin/preview-ascii.sh
+++ common/scripts/.bin/preview-ascii.sh
@@ -2,4 +2,5 @@
 
 # killall entr
 
-ls "$NOTE/$1" | entr -n asciidoctor -o "$NOTE/preview.html" -a source-highlighter=highlightjs -a hardbreaks "$NOTE/$1" &
+ls "$NOTE/$1" | entr -n asciidoctor -o "$NOTE/preview.html" -a source-highlighter=highlightjs -a source-language=java -a highlightjs-languages=java,js,lua,sql,css,html,typescript,kotlin -a hardbreaks -a experimental -a toc=left -a toclevels=5 "$NOTE/$1" &
+
