3b8282332fcf7a5793128ccfaaaa5cba22881610
amiroslaw <arek47@gmail.com>
2022-01-26 18:28:06 +0100

    add tor script; fix autostart

--- common/scripts/.bin/tor.sh
+++ common/scripts/.bin/tor.sh
@@ -1,67 +1,43 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 
-mkdir -p $HOME/.cache/pirokit
+cacheDir="/tmp/tor"
+cacheFile="/tmp/tor/tor.html"
+mkdir -p $cacheDir
 
-if [ -z $1 ]; then
-  query=$(echo "" | dmenu -p "Search Torrent: ")
+if [ -z "$1" ]; then
+  query=$(echo "" | rofi -dmenu -l 0 -p "Search Torrent: ")
 else
   query=$1
 fi
+baseurl="https://pirateproxy.live/search/"
+query="$(sed 's/ /+/g' <<<"$query")"
 
-baseurl="https://1337x.to"
-cachedir="$HOME/.cache/pirokit"
-query="$(sed 's/ /+/g' <<<$query)"
+curl -s $baseurl"$query" > $cacheFile
 
-#curl -s https://1337x.to/category-search/$query/Movies/1/ > $cachedir/tmp.html
-curl -s https://1337x.to/search/$query/1/ > $cachedir/tmp.html
+# Get data
+pup 'tbody tr td div.detName a[title] text{}' -f $cacheFile > $cacheDir/titles
+pup 'tbody tr td font.detDesc text{}' -f $cacheFile > $cacheDir/desc
+awk 'BEGIN{FS=","} {print $2}' $cacheDir/desc | awk 'NF' > $cacheDir/sizes
+pup -p 'tbody tr td:nth-child(2) a:nth-child(2) attr{href}' -f $cacheFile > $cacheDir/magnets
+pup 'tbody tr td:nth-child(3) text{}' -f $cacheFile > $cacheDir/seeds
+pup 'tbody tr td:nth-child(4) text{}' -f $cacheFile > $cacheDir/leeches
 
-# Get Titles
-grep -o '<a href="/torrent/.*</a>' $cachedir/tmp.html |
-  sed 's/<[^>]*>//g' > $cachedir/titles.bw
-
-result_count=$(wc -l $cachedir/titles.bw | awk '{print $1}')
-if [ "$result_count" -lt 1 ]; then
-  notify-send "😔 No Result found. Try again 🔴"
-  exit 0
-fi
-
-
-# Seeders and Leechers
-grep -o '<td class="coll-2 seeds.*</td>\|<td class="coll-3 leeches.*</td>' $cachedir/tmp.html |
-  sed 's/<[^>]*>//g' | sed 'N;s/\n/ /' > $cachedir/seedleech.bw
-
-# Size
-grep -o '<td class="coll-4 size.*</td>' $cachedir/tmp.html |
-  sed 's/<span class="seeds">.*<\/span>//g' |
-  sed -e 's/<[^>]*>//g' > $cachedir/size.bw
-
-# Links
-grep -E '/torrent/' $cachedir/tmp.html |
-  sed -E 's#.*(/torrent/.*)/">.*/#\1#' |
-  sed 's/td>//g' > $cachedir/links.bw
-
-
-
-# Clearning up some data to display
-sed 's/\./ /g; s/\-/ /g' $cachedir/titles.bw |
-  sed 's/[^A-Za-z0-9 ]//g' | tr -s " " > $cachedir/tmp && mv $cachedir/tmp $cachedir/titles.bw
-
-awk '{print NR " - ["$0"]"}' $cachedir/size.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/size.bw
-awk '{print "[S:"$1 ", L:"$2"]" }' $cachedir/seedleech.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/seedleech.bw
+# # Clearning up some data to display
+awk '{print NR "- [S:"$0 ","}' $cacheDir/seeds > $cacheDir/tmp && mv $cacheDir/tmp $cacheDir/seeds
+awk '{print "L:"$0"]" }' $cacheDir/leeches > $cacheDir/tmp && mv $cacheDir/tmp $cacheDir/leeches
 
 # Getting the line number
-LINE=$(paste -d\   $cachedir/size.bw $cachedir/seedleech.bw $cachedir/titles.bw |
-  dmenu -i -l 52 |
-  cut -d\- -f1 |
-  awk '{$1=$1; print}')
+LINE=$(paste -d\  $cacheDir/seeds $cacheDir/leeches $cacheDir/sizes $cacheDir/titles |
+	# dmenu -i -l 52 |
+rofi -dmenu -theme-str 'window {width:  100%;}' -monitor -4 -l 30 |
+  awk 'BEGIN{FS="-"} {print $1}')
 
-  # rofi -dmenu -i -l 25 |
-url=$(head -n $LINE $cachedir/links.bw | tail -n +$LINE)
-fullURL="${baseurl}${url}/"
-
-# Requesting page for magnet link
-curl -s $fullURL > $cachedir/tmp.html
-magnet=$(grep -o -m1 'magnet:.*" ' $cachedir/tmp.html | sed 's/..$//')
+if [ -z "$LINE" ]; then
+  notify-send "😔 No Result selected. Exiting... 🔴" -i "NONE"
+  exit 0
+fi
+notify-send "🔍 Searching Magnet seeds 🧲" -i "NONE"
+magnet=$(head -n $LINE $cacheDir/magnets | tail -n +$LINE)
 
 url.lua tor "$magnet"
-# magnet.sh "$magnet"
+# # magnet.sh "$magnet"
