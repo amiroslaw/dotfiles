ac214fbc6bda1eb08ec674cd8bb37b4bdba41245
amiroslaw <arek47@gmail.com>
2022-03-06 17:19:40 +0100

    add some fzf keybindings; refactor zshrc

--- common/fzf/.config/fzf/shell/key-bindings.zsh
+++ common/fzf/.config/fzf/shell/key-bindings.zsh
@@ -1,15 +1,71 @@
+#     ____      ____
+#    / __/___  / __/
+#   / /_/_  / / /_
+#  / __/ / /_/ __/
+# /_/   /___/_/ key-bindings.zsh
+# Fork from https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
+
+# -------------------------------------------------------------------------
+#                       shortcuts
+# -------------------------------------------------------------------------
+# ALT-F - Paste the selected file path(s) into the command line
+# ALT-D - cd into the selected directory
+# ALT-O - Open or edit the file
+# ALT-E - Open or edit the file from fasd
+# ALT-J - cd into the selected directory from fasd
+# ALT-H - Paste the selected command from history into the command line
+# ALT-Q - Kill process
+# ALT-W - Run application
+# ALT-V - Edit line in vim with ctrl-e: oh-my-zsh do it by esc; v
+
+# bindkey '\ef' → alt '^F' ctr
+# global
+export FZF_DEFAULT_OPTS='--height 80% --layout=reverse --border --bind alt-j:down,alt-k:up,alt-l:accept'
+export FZF_DEFAULT_COMMAND='fd'
+
+export FZF_PATH_COMMAND='fd'
+export FZF_FILE_COMMAND='fd --type f'
+export FZF_DIR_COMMAND='fd --type d'
+export FZF_FILE_OPTS='--preview "bat --style=numbers --color=always --line-range :500 {}"'
+# export FZF_DIR_OPTS
+CMD=''
+OPTS=''
+
+# The code at the top and the bottom of this file is the same as in completion.zsh.
+# Refer to that file for explanation.
+if 'zmodload' 'zsh/parameter' 2>'/dev/null' && (( ${+options} )); then
+  __fzf_key_bindings_options="options=(${(j: :)${(kv)options[@]}})"
+else
+  () {
+    __fzf_key_bindings_options="setopt"
+    'local' '__fzf_opt'
+    for __fzf_opt in "${(@)${(@f)$(set -o)}%% *}"; do
+      if [[ -o "$__fzf_opt" ]]; then
+        __fzf_key_bindings_options+=" -o $__fzf_opt"
+      else
+        __fzf_key_bindings_options+=" +o $__fzf_opt"
+      fi
+    done
+  }
+fi
+
+'emulate' 'zsh' '-o' 'no_aliases'
+{
+
+[[ -o interactive ]] || return 0
+
 # Key bindings
 # ------------
-if [[ $- == *i* ]]; then
 
-# CTRL-f - Paste the selected file path(s) into the command line
+# ALT-F - Paste the selected file path(s) into the command line
 __fsel() {
-  local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . \\( -path '*/\\.*' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+  local cmd="${CMD:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
     -o -type f -print \
     -o -type d -print \
-    -o -type l -print 2> /dev/null | sed 1d | cut -b3-"}"
-  setopt localoptions pipefail 2> /dev/null
-  eval "$cmd | $(__fzfcmd) -m $FZF_CTRL_T_OPTS" | while read item; do
+    -o -type l -print 2> /dev/null | cut -b3-"}"
+  setopt localoptions pipefail no_aliases 2> /dev/null
+  local item
+  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $OPTS" $(__fzfcmd) -m "$@" | while read item; do
     echo -n "${(q)item} "
   done
   local ret=$?
@@ -18,38 +74,58 @@ __fsel() {
 }
 
 __fzfcmd() {
-  [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+  [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; } &&
+    echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
 }
 
 fzf-file-widget() {
+	CMD="$FZF_FILE_COMMAND"
+	OPTS="$FZF_FILE_OPTS"
   LBUFFER="${LBUFFER}$(__fsel)"
   local ret=$?
-  zle redisplay
-  typeset -f zle-line-init >/dev/null && zle zle-line-init
+  zle reset-prompt
   return $ret
 }
 zle     -N   fzf-file-widget
-bindkey '^F' fzf-file-widget
+bindkey '\ef' fzf-file-widget
+
+fzf-path-widget() {
+	CMD="$FZF_PATH_COMMAND"
+  LBUFFER="${LBUFFER}$(__fsel)"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
+zle     -N   fzf-path-widget
+bindkey '\ep' fzf-path-widget
 
 # ALT-D - cd into the selected directory
 fzf-cd-widget() {
-  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . \\( -path '*/\\.*' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-    -o -type d -print 2> /dev/null | sed 1d | cut -b3-"}"
-  setopt localoptions pipefail 2> /dev/null
-  cd "${$(eval "$cmd | $(__fzfcmd) +m $FZF_ALT_C_OPTS"):-.}"
+  local cmd="${FZF_DIR_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type d -print 2> /dev/null | cut -b3-"}"
+  setopt localoptions pipefail no_aliases 2> /dev/null
+  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_CD_OPTS" $(__fzfcmd) +m)"
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+  zle push-line # Clear buffer. Auto-restored on next prompt.
+  BUFFER="cd -- ${(q)dir}"
+  zle accept-line
   local ret=$?
+  unset dir # ensure this doesn't end up appearing in prompt expansion
   zle reset-prompt
-  typeset -f zle-line-init >/dev/null && zle zle-line-init
   return $ret
 }
 zle     -N    fzf-cd-widget
-bindkey '^D' fzf-cd-widget
+bindkey '\ed' fzf-cd-widget
 
-# CTRL-H - Paste the selected command from history into the command line
+# ALT-H - Paste the selected command from history into the command line
 fzf-history-widget() {
   local selected num
-  setopt localoptions noglobsubst pipefail 2> /dev/null
-  selected=( $(fc -l 1 | eval "$(__fzfcmd) +s --tac +m -n2..,.. --tiebreak=index --toggle-sort=ctrl-r $FZF_CTRL_R_OPTS -q ${(q)LBUFFER}") )
+  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+  selected=( $(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
+    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
   local ret=$?
   if [ -n "$selected" ]; then
     num=$selected[1]
@@ -57,22 +133,87 @@ fzf-history-widget() {
       zle vi-fetch-history -n $num
     fi
   fi
-  zle redisplay
-  typeset -f zle-line-init >/dev/null && zle zle-line-init
+  zle reset-prompt
   return $ret
 }
 zle     -N   fzf-history-widget
-bindkey '^H' fzf-history-widget
+bindkey '\eh' fzf-history-widget
 
-fi
+# -------------------------------------------------------------------------
+#                       CUSTOM
+# -------------------------------------------------------------------------
+# ALT-Q - Kill process
+fzf_killps() { 
+	zle -I; 
+	ps -ef | sed 1d | fzf -m | awk '{print $2}' | xargs kill -${1:-9}; 
+} 
+zle -N fzf_killps;
+bindkey '\eq' fzf_killps
 
-## custom
-#ctrl-q procesy 
-fzf_killps() { zle -I; ps -ef | sed 1d | fzf -m | awk '{print $2}' | xargs kill -${1:-9} ; }; zle -N fzf_killps;
-bindkey '^Q' fzf_killps
+#ALT-w apps list 
 fzf-dmenu() { 
     # note: xdg-open has a bug with .desktop files, so we cant use that shit
     selected="$(ls /usr/share/applications | fzf -e)"
     nohup `grep '^Exec' "/usr/share/applications/$selected" | tail -1 | sed 's/^Exec=//' | sed 's/%.//'` >/dev/null 2>&1&
 }
-bindkey -s '^W' "fzf-dmenu\n"
+zle -N fzf-dmenu;
+bindkey '\ew' fzf-dmenu
+
+# Edit line in vim with ctrl-e: oh-my-zsh do it by esc; v
+autoload edit-command-line; zle -N edit-command-line
+bindkey '\ev' edit-command-line
+
+# open 
+# support swallow with devour 
+# Modified version where you can press
+#   - CTRL-O to open with `open` command, edit to swallow
+#   - CTRL-E or Enter key to open with the $EDITOR
+fzf_open() {
+	zle -I;
+ FZF_CMD="fzf $FZF_DEFAULT_OPTS $FZF_FILE_OPTS --query="$1" --exit-0 --expect=alt-o,ctrl-e --prompt='alt-o→open;else→edit >'"
+  IFS=$'\n' out=("$(eval "fasd -Rfl | $FZF_CMD" )")
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = alt-o ] && devour xdg-open "$file" || ${EDITOR:-vim} "$file"
+  fi
+}
+zle     -N   fzf_open;
+bindkey '\eo' fzf_open
+
+fzf_fasd_open() {
+	zle -I;
+ FZF_CMD="fzf $FZF_DEFAULT_OPTS $FZF_FILE_OPTS --query="$1" --exit-0 --expect=alt-o,ctrl-e --prompt='alt-o→open;else→edit >'"
+  IFS=$'\n' out=("$(eval "fasd -Rfl | $FZF_CMD" )")
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = alt-o ] && devour xdg-open "$file" || ${EDITOR:-vim} "$file"
+  fi
+}
+zle     -N   fzf_fasd_open
+bindkey '\ee' fzf_fasd_open
+
+fzf_fasd_cd() {
+	zle -I;
+  IFS=$'\n' dir=("$(fasd -Rdl | fzf --query="$1" --exit-0 )")
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+  zle push-line # Clear buffer. Auto-restored on next prompt.
+  BUFFER="cd -- ${(q)dir}"
+  zle accept-line
+  local ret=$?
+  unset dir # ensure this doesn't end up appearing in prompt expansion
+  zle reset-prompt
+  return $ret
+}
+zle     -N   fzf_fasd_cd;
+bindkey '\ej' fzf_fasd_cd
+
+
+} always {
+  eval $__fzf_key_bindings_options
+  'unset' '__fzf_key_bindings_options'
+}
--- common/zsh/.zshrc
+++ common/zsh/.zshrc
@@ -6,10 +6,7 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 source ~/.config/fzf/fzf.zsh
 source ~/.config/broot/launcher/bash/br
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source "${HOME}/.config/zgenom/sources/urbainvaes/fzf-marks/___/fzf-marks.plugin.zsh"
-# FZF_MARKS_JUMP="^z"
-source $HOME/.config/aliases
+source "${HOME}/.config/aliases"
 
 # -------------------------------------------------------------------------
 #                       # Options section
@@ -33,7 +30,9 @@ zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
 
 # expand alias with TAB
-zstyle ':completion:*' completer _expand_alias _complete _ignored
+zstyle ':completion:*' menu select
+zmodload -i zsh/complist
+# zstyle ':completion:*' completer _expand_alias _complete _ignored
 # bindkey "mykeybinding" _expand_alias
 
 # -------------------------------------------------------------------------
@@ -52,79 +51,39 @@ SAVEHIST=$HISTSIZE
 export HISTCONTROL=ignoreboth
 
 # -------------------------------------------------------------------------
+#                       # ZGENOM
+# -------------------------------------------------------------------------
+source "${HOME}/.config/zgenom/zgenom.zsh"
+if ! zgenom saved; then
+	# zgenom ohmyzsh #adds all aliases
+	zgenom ohmyzsh plugins/sudo 
+	zgenom ohmyzsh plugins/history 
+	zgenom ohmyzsh plugins/web-search 
+	zgenom ohmyzsh plugins/colored-man-pages
+	# zgenom ohmyzsh plugins/git 
+	# zgenom ohmyzsh plugins/fasd # has error
+	zgenom load wookayin/fzf-fasd
+	zgenom load zsh-users/zsh-completions
+	zgenom load zsh-users/zsh-autosuggestions
+	zgenom load zsh-users/zsh-syntax-highlighting
+	zgenom load romkatv/powerlevel10k powerlevel10k
+	zgenom load jeffreytse/zsh-vi-mode # can override some keybindings
+	zgenom load arzzen/calc.plugin.zsh
+	zgenom load ChrisPenner/copy-pasta
+	zgenom load urbainvaes/fzf-marks # mark alias_name ctrl-g
+	zgenom load ptavares/zsh-sdkman
+
+	zgenom save
+    zgenom compile "$HOME/.zshrc" # Compile your zsh files
+fi
+
+# -------------------------------------------------------------------------
 #                       bindkey
 # -------------------------------------------------------------------------
 # autosuggestions
 bindkey '^l' autosuggest-accept
 bindkey '^j' autosuggest-execute
 bindkey '^ ' autosuggest-clear
-
-# CTRL-H - Paste the selected command from history into the command line
-fzf-history-widget() {
-  local selected num
-  setopt localoptions noglobsubst pipefail 2> /dev/null
-  selected=( $(fc -l 1 | eval "$(__fzfcmd) +s --tac +m -n2..,.. --tiebreak=index --toggle-sort=ctrl-r $FZF_CTRL_R_OPTS -q ${(q)LBUFFER}") )
-  local ret=$?
-  if [ -n "$selected" ]; then
-    num=$selected[1]
-    if [ -n "$num" ]; then
-      zle vi-fetch-history -n $num
-    fi
-  fi
-  zle redisplay
-  typeset -f zle-line-init >/dev/null && zle zle-line-init
-  return $ret
-}
-zle     -N   fzf-history-widget
-bindkey '^H' fzf-history-widget
-
-# Edit line in vim with ctrl-e: oh-my-zsh do it by esc; v
-	# ctr e w fzf to otwieranie folderow
-autoload edit-command-line; zle -N edit-command-line
-bindkey '^e' edit-command-line
-
-# Change cursor shape for different vi modes.
-	# powerlevel9k inform about mode 
-function zle-keymap-select {
-  if [[ ${KEYMAP} == vicmd ]] ||
-     [[ $1 = 'block' ]]; then
-    echo -ne '\e[1 q'
-  elif [[ ${KEYMAP} == main ]] ||
-       [[ ${KEYMAP} == viins ]] ||
-       [[ ${KEYMAP} = '' ]] ||
-       [[ $1 = 'beam' ]]; then
-    echo -ne '\e[5 q'
-  fi
-}
-
-# -------------------------------------------------------------------------
-#                       # ZGENOM didn't work in the top of the zshrc
-# -------------------------------------------------------------------------
-source "${HOME}/.config/zgenom/zgenom.zsh"
-if ! zgenom saved; then
-	zgenom ohmyzsh
-
-	zgenom ohmyzsh plugins/fasd 
-	zgenom ohmyzsh plugins/sudo 
-	zgenom ohmyzsh plugins/history 
-	zgenom ohmyzsh plugins/web-search 
-	zgenom ohmyzsh plugins/colored-man-pages
-	# zgenom ohmyzsh plugins/git 
-	
-	zgenom load zsh-users/zsh-completions
-	zgenom load zsh-users/zsh-autosuggestions
-	zgenom load zsh-users/zsh-syntax-highlighting
-	zgenom load romkatv/powerlevel10k powerlevel10k
-	zgenom load jeffreytse/zsh-vi-mode
-	zgenom load arzzen/calc.plugin.zsh
-	zgenom load ChrisPenner/copy-pasta
-	zgenom load urbainvaes/fzf-marks
-	zgenom load ptavares/zsh-sdkman
-
-	zgenom save
- # Compile your zsh files
-    zgenom compile "$HOME/.zshrc"
-fi
 
 # Use vim keys in tab complete menu:
 bindkey -M menuselect 'h' vi-backward-char
@@ -136,9 +95,9 @@ bindkey -v '^?' backward-delete-char
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+# To customize prompt, run `p10k configure` or edit ~/.config/p10k.zsh.
 [[ ! -f ~/.config/p10k.zsh ]] || source ~/.config/p10k.zsh
 
+# Needed it if the plugin is not loaded
+eval "$(fasd --init auto)"
 # shuf -n 1 $CONFIG/logs/dictionary/enpl-dictionary.txt
-# I don't know if i use it
-# eval "$(fasd --init auto)"
