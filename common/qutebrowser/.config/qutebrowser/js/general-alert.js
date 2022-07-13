e374122bffbdd7bbc20ded2f91a6f9eb9fb43ced
amiroslaw <arek47@gmail.com>
2022-07-13 15:41:45 +0200

    qb startpage

--- common/qutebrowser/.config/qutebrowser/js/general-alert.js
+++ common/qutebrowser/.config/qutebrowser/js/general-alert.js
@@ -4,6 +4,8 @@ if(url.includes("pepper")) {
 	document.getElementById("popover2").click();
 } else if(url.includes("youtube")) {
 	document.querySelector('ytd-notification-topbar-button-renderer button').click();
+} else if(url.includes("4programmers")) {
+	document.querySelector('#nav-auth a:first-child').click();
 } else {
 	alert('url pattern not found');
 }
