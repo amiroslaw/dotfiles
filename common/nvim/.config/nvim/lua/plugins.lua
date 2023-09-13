6d0741e2d859bc6e78b0fefc98219f547c2c4d58
amiroslaw <arek47@gmail.com>
2023-09-14 01:42:15 +0200

    minor changes; btop

--- common/nvim/.config/nvim/lua/plugins.lua
+++ common/nvim/.config/nvim/lua/plugins.lua
@@ -50,7 +50,7 @@ return {
 		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
 		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
 		{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-		{ "<LocalLeader>l", mode = { "n", "o", "x" }, function() require("flash").jump( {
+		{ "<LocalLeader>l", mode = { "n", "o", "x" }, function() require("flash").jump( { --IDK if it work without treesitter
 			  search = { mode = "search", max_length = 0 },
 			  label = { after = { 0, 0 } },
 			  pattern = "^"
