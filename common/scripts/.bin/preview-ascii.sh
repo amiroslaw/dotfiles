70c935e5dfb3eca05d2d5a4624db109be7eed760
amiroslaw <arek47@gmail.com>
2025-02-17 00:02:11 +0100

    fix preview-ascii.sh

--- common/scripts/.bin/preview-ascii.sh
+++ common/scripts/.bin/preview-ascii.sh
@@ -2,9 +2,13 @@
 
 killall entr
 
-ls "$1" | entr -n asciidoctor -o "$NOTE/preview.html" -a source-highlighter=highlight.js -a source-language=java -a highlightjs-languages=java,js,lua,sql,css,html,typescript,kotlin -a hardbreaks -a experimental=true -a toc=left -a toclevels=5 -a icons=font@ -a allow-uri-read=true -a backend=html5 -a sectanchors=true -a sectlinks=true -a table-stripes=even -r asciidoctor-diagram -a imagesoutdir=./img -a imagesdir=./img -a plantuml-format=svg "$1" &
+ls "$1" | entr -n asciidoctor -o "$NOTE/preview.html" -a source-highlighter=highlight.js -a source-language=java -a highlightjs-languages=java,js,lua,sql,css,html,typescript,kotlin -a hardbreaks -a experimental=true -a toc=left -a toclevels=5 -a icons=font@ -a allow-uri-read=true -a backend=html5 -a sectanchors=true -a sectlinks=true -a table-stripes=even  -a imagesoutdir=./img -a imagesdir=./img -a plantuml-format=svg "$1" &
+
+# bug with asciidoctor-diagram
+# ls "$1" | entr -n asciidoctor -o "$NOTE/preview.html" -a source-highlighter=highlight.js -a source-language=java -a highlightjs-languages=java,js,lua,sql,css,html,typescript,kotlin -a hardbreaks -a experimental=true -a toc=left -a toclevels=5 -a icons=font@ -a allow-uri-read=true -a backend=html5 -a sectanchors=true -a sectlinks=true -a table-stripes=even -r asciidoctor-diagram -a imagesoutdir=./img -a imagesdir=./img -a plantuml-format=svg "$1" &
 
 "$BROWSER" "$NOTE/preview.html" &
+# --trace debug
 
 # ls "$1" | entr -n asciidoctor -o "$NOTE/preview.html" -a source-highlighter=highlight.js -a source-language=java -a highlightjs-languages=java,js,lua,sql,css,html,typescript,kotlin -a hardbreaks -a experimental=true -a toc=left -a toclevels=5 -a icons=font@ -a allow-uri-read=true "$1" &
 
