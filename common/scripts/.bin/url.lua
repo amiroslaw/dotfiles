60722e3bc1888e96567ca7aa007c1c30caef0124
amiroslaw <arek47@gmail.com>
2025-05-24 00:23:57 +0200

    feat(yt-dlp): add -q flag

--- common/scripts/.bin/url.lua
+++ common/scripts/.bin/url.lua
@@ -177,7 +177,7 @@ end
 
 local function dlAudio(linkTab)
 	os.execute('mkdir -p ' .. AUDIO_DIR)
-	local cmdDlp = 'yt-dlp --embed-metadata -f bestaudio -x --audio-format mp3 -o "' .. AUDIO_DIR .. '%%(title)s.%%(ext)s" '
+	local cmdDlp = 'yt-dlp -q --embed-metadata -f bestaudio -x --audio-format mp3 -o "' .. AUDIO_DIR .. '%%(title)s.%%(ext)s" '
 	local cmd = 'pueue add --escape --label "%s" -g dl-audio -- ' .. cmdDlp
 	M(linkTab):map(getTitle)
 		:each(M.bindn(execQueue, cmd, 'dl-audio'))
@@ -185,7 +185,7 @@ end
 
 local function dlVideo(linkTab)
 	os.execute('mkdir -p ' .. YT_DIR)
-	local cmdDlp = "yt-dlp --embed-metadata -f 'bestvideo[height<=1440]+bestaudio/best' -o '" .. YT_DIR .. "%%(title)s.%%(ext)s' "
+	local cmdDlp = "yt-dlp -q --embed-metadata -f 'bestvideo[height<=1440]+bestaudio/best' -o '" .. YT_DIR .. "%%(title)s.%%(ext)s' "
 	local cmd = 'pueue add --escape --label "%s" -g dl-video -- ' .. cmdDlp
 	M(linkTab):map(getTitle)
 		:each(M.bindn(execQueue, cmd, 'dl-video'))
